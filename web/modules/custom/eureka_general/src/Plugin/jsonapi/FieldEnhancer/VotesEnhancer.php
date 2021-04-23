81368534c5a74426c3f1ebd3029466afa6ce7ae5
Jeremy Declerck <jeremy.declerck@district01.be>
2021-04-23 12:51:16 +0200

    Add some comments.

--- web/modules/custom/eureka_general/src/Plugin/jsonapi/FieldEnhancer/VotesEnhancer.php
+++ web/modules/custom/eureka_general/src/Plugin/jsonapi/FieldEnhancer/VotesEnhancer.php
@@ -38,28 +38,36 @@ class VotesEnhancer extends ResourceFieldEnhancerBase implements ContainerFactor
   protected function doUndoTransform($data, Context $context) {
     /** @var \Drupal\votingapi\VoteResultFunctionManager $voting_service */
     $voting_service = \Drupal::service('plugin.manager.votingapi.resultfunction');
+
+    // Get the node's UUID out of the route.
     $uuid = \Drupal::routeMatch()->getRawParameter('entity');
+
     /** @var \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository */
     $entity_repository = \Drupal::service('entity.repository');
     $entity = $entity_repository->loadEntityByUuid('node', $uuid);
+
+    // Get the votes of the current node.
     $votes = $voting_service->getResults('node', $entity->id());
     $votes = reset($votes);
-    $voties = \Drupal::entityTypeManager()->getStorage('vote')->loadByProperties([
+
+    // Look for existing votes on the node by the current user.
+    $user_votes = \Drupal::entityTypeManager()->getStorage('vote')->loadByProperties([
       'type' => 'updown',
       'entity_id' => $entity->id(),
       'user_id' => \Drupal::currentUser()->id(),
     ]);
 
     /** @var \Drupal\votingapi\Entity\Vote $votie */
-    $votie = reset($voties);
+    $user_vote = reset($user_votes);
 
+    // The data we want to return.
     $data = [
       'total' => $votes['vote_count'],
       'average' => $votes['vote_average'],
       'sum' => $votes['vote_sum'],
       'count_up' => $votes['rate_count_up'],
-      'user_vote_id' => $votie ? $votie->id() : '',
-      'user_vote_source' => $votie ? $votie->getSource() : '',
+      'user_vote_id' => $user_vote ? $user_vote->id() : '',
+      'user_vote_source' => $user_vote ? $user_vote->getSource() : '',
     ];
 
     return $data;
